60f7fce39bc9a5d96cc50155fc27bbdb4484de2b
Felix Stephen <felsen88@gmail.com>
2019-03-22 16:17:26 +0545

    Modified: changes for two sums

--- problems/two_sum.py
+++ problems/two_sum.py
@@ -1,9 +1,19 @@
-# num, target = [2, 7, 11, 15], 9
+num, target = [2, 7, 11, 15], 9
 
-# num, target = [3, 2, 4], 6
+num, target = [3, 2, 4], 6
 
 num, target = [3, 3], 6
 
+num, target = [3, 2, 4], 6
+
+# Way 1
+for i in num:
+    value = target - i
+    if value in num and not (idx == num.index(value)):
+        print(idx, num.index(value))
+        break
+
+# Way 2
 num_restruct = list(range(len(num)))
 a = [[x, y] for x in num_restruct for y in num_restruct if not x == y]
 
